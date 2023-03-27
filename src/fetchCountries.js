48c3d7729a4e90a507b494763931ffb27cfc465d
OlesjaD <olesja.d.c@gmail.com>
2023-03-28 01:50:57 +0300

    error status

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(name) {
    return fetch('${BASE_URL}/name/${valueCountry}?fields=name,capital,population,flags,languages')
-    .then(response => response.json())
+    .then(response => { if(!response.ok) {
+        throw new Error(response.status)}
+    return response.json()}).catch(console.log('ERROR!!!'))
 }
 
 export default {fetchCountries};
